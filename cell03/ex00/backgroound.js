ed127123945856b3f564389746576bb859dfeced
/ᐠ. ｡.ᐟ\ᵐᵉᵒʷˎˊ˗ <thanapon9114@gmail.com>
2025-09-10 23:20:26 +0700

    cell3-0

---
+++ cell03/ex00/backgroound.js
@@ -0,0 +1,11 @@
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return color;
+}
+document.getElementById("btn").addEventListener("click", function event() {
+        document.body.style.backgroundColor = getRandomColor();
+    });
